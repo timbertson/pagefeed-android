729f6019b6dba6f54ebc8e3c391fe510aca51b9f
gfxmonk <tim3d.junk@gmail.com>
2010-05-24 22:47:34 +1000

    java version of auth; because scala cant implement varargs interfaces in 2.7

---
+++ src/main/java/net/gfxmonk/android/pagefeed/auth/AccountList.java
@@ -0,0 +1,35 @@
+package net.gfxmonk.android.pagefeed.auth;
+
+import net.gfxmonk.android.pagefeed.R;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AccountList extends ListActivity {
+	protected AccountManager accountManager;
+	protected Intent intent;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		accountManager = AccountManager.get(getApplicationContext());
+		Account[] accounts = accountManager.getAccountsByType("com.google");
+		this.setListAdapter(new ArrayAdapter<Account>(this, R.layout.list_item, accounts));
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Account account = (Account)getListView().getItemAtPosition(position);
+		Intent intent = new Intent(this, AppInfo.class);
+		intent.putExtra("account", account);
+		intent.putExtra("baseUrl", "https://pagefeed.appspot.com");
+		startActivity(intent);
+	}
+}
