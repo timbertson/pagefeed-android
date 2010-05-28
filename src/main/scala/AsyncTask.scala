374e50107694530b57422e7977db036b910137e8
gfxmonk <tim3d.junk@gmail.com>
2010-05-28 19:20:03 +1000

    ?

---
+++ src/main/scala/AsyncTask.scala
@@ -0,0 +1,21 @@
+package net.gfxmonk.android.pagefeed
+
+class AsyncTask[InType, ProgressType, OutType](f:(InType)=>OutType) extends MyAsyncTask[InType, ProgressType, OutType]{
+	var postExecute: (OutType) => Unit = null
+	override protected[pagefeed] def doInBackground(inVal: InType):OutType = {
+		return f(inVal)
+	}
+
+	override protected[pagefeed] def onPostExecute(result:OutType):Unit = {
+		if (postExecute != null) {
+			postExecute(result)
+		}
+		null
+	}
+
+	def andThen(andThenFunc:(OutType) => Unit) = {
+		postExecute = andThenFunc
+		this
+	}
+}
+
